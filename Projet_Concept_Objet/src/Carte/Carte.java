1eb601b88980e4853c812fb566eff68bb570cbbe
CharlesP-0 <56159744+CharlesP-0@users.noreply.github.com>
2020-10-09 09:15:37 +0200

    Ajout de l'affichage dela carte + couleur d'affichage en fonction de l'entité présente (safezone, obstacle ou personnage)

--- Projet_Concept_Objet/src/Carte/Carte.java
+++ Projet_Concept_Objet/src/Carte/Carte.java
@@ -1,11 +1,23 @@
 package Carte;
 
+import Personnage.EfleNoir;
 import Personnage.Entity;
+import Personnage.Khajit;
+import Personnage.Nordique;
+import Personnage.Orcs;
 
 public class Carte {
 	private Colonne[] carte;
 	private int nbColonne;
 	private int nbLigne;
+	//Penser à ajouter le plugin https://marketplace.eclipse.org/content/ansi-escape-console#group-detailsgithub.com/fusesource/jansi
+	//Sinon les couleurs ne s'afficheront pas pour les prints
+	private String red = "\033[31m";
+	private String blue = "\033[34m";
+	private String green = "\033[32m";
+	private String yellow = "\033[33m";
+	private String purple = "\033[35m";
+	private String black = "\u001B[0m";
 
 	public Carte(int ligne, int colonne) {
 		this.nbColonne = colonne;
@@ -28,14 +40,16 @@ public class Carte {
 					this.carte[i].getCases()[j].setSafeZone(ville);
 				}
 			}
-			System.out.println(String.format("Les cases comprises entre [%d,%d] et [%d,%d] ont été attribuées comme safezone pour "+ville+" .",departX, departY, finX, finY));
+			System.out.println(
+					String.format("Les cases comprises entre [%d,%d] et [%d,%d] ont été attribuées comme safezone pour "
+							+ ville + " .", departX, departY, finX, finY));
 		}
 	}
 
 	public void setOccupation(int x, int y, Entity entity) {
 		this.carte[x].getCases()[y].setOccupied(true);
 		this.carte[x].getCases()[y].setIsOccupiedBy(entity);
-		System.out.println(String.format("Entity "+entity+" as been set at [%d,%d]", x, y));
+		System.out.println(String.format("Entity " + entity + " as been set at [%d,%d]", x, y));
 	}
 
 	public void isOccupied(int x, int y) {
@@ -51,9 +65,45 @@ public class Carte {
 		boolean isASafezone = this.carte[x].getCases()[y].isASafeZone();
 		if (isASafezone) {
 			SafeZone safezone = this.carte[x].getCases()[y].getSafeZone();
-			System.out.println(String.format("La case [%d,%d] est une safezone de "+ safezone, x, y));
+			System.out.println(String.format("La case [%d,%d] est une safezone de " + safezone, x, y));
 		} else {
 			System.out.println(String.format("La case [%d,%d] n'est pas une safezone", x, y));
+		}
+	}
+
+	public void displayMap() {
+		for (int i = 0; i < this.nbLigne; i++) {
+			for (int j = 0; j < this.nbColonne; j++) {
+				if (this.carte[j].getCases()[i].isASafeZone()) {
+					if (this.carte[j].getCases()[i].getSafeZone() == SafeZone.EPERVINE) {
+						System.out.print(red + " E" + black);
+					} else if (this.carte[j].getCases()[i].getSafeZone() == SafeZone.MARKATH) {
+						System.out.print(green +" M"+ black);
+					} else if (this.carte[j].getCases()[i].getSafeZone() == SafeZone.FORTDHIVER) {
+						System.out.print(blue+" F"+black);
+					} else if (this.carte[j].getCases()[i].getSafeZone() == SafeZone.SOLITUDE) {
+						System.out.print(purple+" S"+black);
+					}
+				} else if (this.carte[j].getCases()[i].isOccupied()) {
+					if (this.carte[j].getCases()[i].getIsOccupiedBy().getClass() == Khajit.class) {
+						System.out.print(blue+" K"+black);
+					} else if (this.carte[j].getCases()[i].getIsOccupiedBy().getClass() == EfleNoir.class) {
+						System.out.print(purple+" N"+black);
+					} else if (this.carte[j].getCases()[i].getIsOccupiedBy().getClass() == Nordique.class) {
+						System.out.print(green+" N"+black);
+					} else if (this.carte[j].getCases()[i].getIsOccupiedBy().getClass() == Orcs.class) {
+						System.out.print(red+" O"+black);
+					} else {
+						System.out.print(yellow+" X"+black);
+					}
+				} else {
+					System.out.print(" .");
+				}
+				if (j != (this.nbColonne - 1)) {
+					System.out.print(" -");
+				}
+			}
+			System.out.println();
 		}
 	}
 }
--- Projet_Concept_Objet/src/Main/Simulation.java
+++ Projet_Concept_Objet/src/Main/Simulation.java
@@ -14,22 +14,22 @@ public class Simulation {
 		System.out.println("je suis un projet java");
 		
 		MaitreOrc.getInstance();
-		/*Test de la carte
+		//Test de la carte
 		Carte carte = new Carte (20,20);
 		Khajit test = new Khajit();
 		carte.setCaseAsSafeZone(0,0, 2, 3, SafeZone.EPERVINE);
-		TimeUnit.SECONDS.sleep(1);
+		//TimeUnit.SECONDS.sleep(1);
 		carte.setOccupation(5, 3, test);
-		TimeUnit.SECONDS.sleep(1);
+		//TimeUnit.SECONDS.sleep(1);
 		carte.isOccupied(5, 3);
-		TimeUnit.SECONDS.sleep(1);
+		//TimeUnit.SECONDS.sleep(1);
 		carte.isOccupied (13,3);
-		TimeUnit.SECONDS.sleep(1);
+		//TimeUnit.SECONDS.sleep(1);
 		carte.isASafeZone(1,1);
-		TimeUnit.SECONDS.sleep(1);
+		//TimeUnit.SECONDS.sleep(1);
 		carte.isASafeZone(7, 12);
-		TimeUnit.SECONDS.sleep(1);
-		*/
+		//TimeUnit.SECONDS.sleep(1);
+		carte.displayMap();
 	}
 
 }
