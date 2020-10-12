7106c5895fba06ede9d87deab6de907870a31993
SombreWolf <mathiascarbor@gmail.com>
2020-10-12 14:36:01 +0200

    Gestion Message basique

---
+++ Projet_Concept_Objet/src/Message/Message.java
@@ -0,0 +1,17 @@
+package Message;
+
+public class Message {
+	private String content;
+	
+	public Message() {
+		setContent("Hello!");
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
--- Projet_Concept_Objet/src/Personnage/Personnage.java
+++ Projet_Concept_Objet/src/Personnage/Personnage.java
@@ -1,23 +1,110 @@
 package Personnage;
 
 import java.util.List;
+import java.util.Random;
+
+import Message.Message;
 
 public abstract class Personnage extends Entity{
 	private String name;
 	private int pointAction;
-	private List<String> messages;
+	private Message[] messages;
+	private List<Message> messagesReceived;
 	private String lastDirection;
+	private int alliance;
 	boolean isOutOfAP; 
+	
+	public Personnage() {
+		
+	}
 	
 	public void move() {
 	}
 	
-	public void meet() {
+	public void meet(Personnage personnage) {
+		//confrontation between the two
+		boolean sameAlliance = this.alliance==personnage.getAlliance();
+		if(sameAlliance) {
+			this.takeMsgFrom(personnage, sameAlliance);
+			personnage.takeMsgFrom(this, sameAlliance);
+		}
+		else {
+			if(this.pointAction > personnage.getPointAction()) {
+				this.takeMsgFrom(personnage, sameAlliance);
+			}
+			else {
+				personnage.takeMsgFrom(this, sameAlliance);
+			}
+		}
 	}
 	
-	public void takeMsg() {	
+	public void takeMsgFrom(Personnage peronnage, boolean sameAlliance) {
+		Message messageReceived = giveMsg();
+		this.messagesReceived.add(messageReceived);
+		
 	}
 	
-	public void giveMsg() {
+	public Message giveMsg() {
+		Random gen = new Random(this.messages.length);
+		return this.messages[gen.nextInt(this.messages.length)];
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPointAction() {
+		return pointAction;
+	}
+
+	public void setPointAction(int pointAction) {
+		this.pointAction = pointAction;
+	}
+
+	public Message[] getMessages() {
+		return messages;
+	}
+
+	public void setMessages(Message[] messages) {
+		this.messages = messages;
+	}
+
+	public List<Message> getMessagesReceived() {
+		return messagesReceived;
+	}
+
+	public void setMessagesReceived(List<Message> messagesReceived) {
+		this.messagesReceived = messagesReceived;
+	}
+
+	public String getLastDirection() {
+		return lastDirection;
+	}
+
+	public void setLastDirection(String lastDirection) {
+		this.lastDirection = lastDirection;
+	}
+
+	public int getAlliance() {
+		return alliance;
+	}
+
+	public void setAlliance(int alliance) {
+		this.alliance = alliance;
+	}
+
+	public boolean isOutOfAP() {
+		return isOutOfAP;
+	}
+
+	public void setOutOfAP(boolean isOutOfAP) {
+		this.isOutOfAP = isOutOfAP;
+	}
+
+	
+	
 }
